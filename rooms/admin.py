1a8d332ca6d7202d385d3340c113190be559af8a
jy1987 <87jy1015@gmail.com>
2021-05-31 01:12:27 +0900

    rename HouseRule from Rule and add Photo

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,14 +2,26 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+# 각각 가져야할 조건이나 속성들이 다르므로 세개로 나눴다.
 
 
-@admin.register(models.RoomType, models.Rule, models.Amenity, models.Facility)
+@admin.register(models.RoomType, models.HouseRule, models.Amenity, models.Facility)
 class ItemAdmin(admin.ModelAdmin):
+    """Item Admin definition"""
+
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    """Room Admin definition"""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ """
 
     pass
--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
+
+## from users import models as user_models
 
 # Create your models here.
 
@@ -23,28 +25,44 @@ class RoomType(AbstractItem):
 
     """Room Type Model definition"""
 
-    pass
+    class Meta:
+        verbose_name = "Room Type"
 
 
 class Amenity(AbstractItem):
 
     """Amenity Model definition"""
 
-    pass
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
 
     """Facility Model definition"""
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 
-class Rule(AbstractItem):
+class HouseRule(AbstractItem):
 
     """HouseRule Model definition"""
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """Photo Model definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -63,11 +81,11 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    house_rules = models.ManyToManyField(Rule)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(
         self,
