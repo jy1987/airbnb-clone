7c205fae363086570e4955e4ad43fd08a4a524c2
jy1987 <87jy1015@gmail.com>
2021-06-24 15:49:13 +0900

    About LoginRequiredMixin and get_success_url

--- users/mixins.py
+++ users/mixins.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.contrib import messages
+from django.urls import reverse_lazy
 
 
 class LoggedOutOnlyView(UserPassesTestMixin):
@@ -10,4 +12,9 @@ class LoggedOutOnlyView(UserPassesTestMixin):
         return not self.request.user.is_authenticated
 
     def handle_no_permission(self):
+        messages.error(self.request, "Can not go there")
         return redirect("core:home")
+
+
+class LoggedInOnlyView(LoginRequiredMixin):
+    login_url = reverse_lazy("users:login")
